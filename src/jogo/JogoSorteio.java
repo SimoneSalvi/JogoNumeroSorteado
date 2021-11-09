362ae18f07815e3b2d49d3cf4946e8f2b3d3c8dc
SimoneSalvi <simonebonemer@yahoo.com.br>
2021-11-09 08:45:04 -0300

    atividade

---
+++ src/jogo/JogoSorteio.java
@@ -0,0 +1,45 @@
+package jogo;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/*
+ Crie um jogo para o usuário descobrir um número sorteado de 1 a 50. 
+ A cada tentativa dele, forneça uma dica mostrando se o número é maior ou menor. 
+ Quando ele descobrir exiba uma mensagem de parabéns e 
+ mostre em quantas tentativas ele conseguiu.
+ */
+
+public class JogoSorteio {
+
+	public static void main(String[] args) {
+		
+        //instância um objeto da classe Random usando o construtor padrão
+        Random gerador = new Random();
+        Scanner leia = new Scanner(System.in);
+		
+
+    //um número inteiro aleatório de 1 a 50
+    for (int i = 1; i <= 1; i++) {
+        int numeroSorteado=gerador.nextInt(51);
+        
+        System.out.println("Bem vindo ao Jogo Número da Sorte! \n"
+        		+ "Neste jogo você deve adivinhar o valor de um número aleatório de 1 a 50. \n"
+        		+ "Pronto para começar? Vamos lá! ");
+        
+        int numeroChutado=0;
+        int cont=0;
+        while (numeroChutado!=numeroSorteado) {
+        	System.out.println("Qual o número sorteado? Chute o valor:");
+        	numeroChutado = leia.nextInt();
+        	cont++;
+        	if(numeroChutado<numeroSorteado) {
+        		System.out.println("Hummm, seu chute está abaixo do valor.. Não desista, vamos continuar!");
+        	}else if (numeroChutado>numeroSorteado) {
+        		System.out.println("Haaaam, seu chute está acima do valor.. Não desista, vamos continuar!");
+        	}
+        }
+        System.out.println("Parabéns, você acertou o número sorteado em " + cont + " tentativas!!");
+    }
+	}
+}
